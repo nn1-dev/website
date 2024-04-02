b00a2fcff0d6280268a4fcecb16b0ec32ca9e18f
Dan Jordan <daniel.jordan@mindera.com>
2024-04-02 11:53:32 +0100

    Update home link

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -11,6 +11,8 @@ function Nav() {
             viewBox="0 0 102 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            role="img"
+            aria-label="NN1 Home"
           >
             <path
               fillRule="evenodd"
