1b7f3978f94a7a3e3544ec3b012fe157b8da710c
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-04-06 08:53:55 +0100

    from next to astro

---
+++ src/content/config.ts
@@ -0,0 +1,41 @@
+import { z, defineCollection } from "astro:content";
+
+const eventsCollection = defineCollection({
+  type: "data",
+  schema: z.object({
+    id: z.number(),
+    name: z.string(),
+    description: z.string(),
+    dateStart: z.coerce.date(),
+    dateEnd: z.coerce.date(),
+    locationText: z.string(),
+    locationUrl: z.string().url(),
+    ticketText: z.string(),
+    ticketUrl: z.string().url(),
+    parkingText: z.string(),
+    parkingUrl: z.string().url(),
+    schedule: z.array(
+      z.object({
+        dateStart: z.coerce.date(),
+        dateEnd: z.coerce.date(),
+        title: z.string(),
+        description: z.string().optional(),
+      }),
+    ),
+    guests: z.array(
+      z.object({
+        name: z.string(),
+        role: z.string(),
+        image: z.string(),
+        urlWebsite: z.string().url().optional(),
+        urlGitHub: z.string().url().optional(),
+        urlMastodon: z.string().url().optional(),
+        urlLinkedIn: z.string().url().optional(),
+        urlInstagram: z.string().url().optional(),
+        urlTwitter: z.string().url().optional(),
+      }),
+    ),
+  }),
+});
+
+export const collections = { events: eventsCollection };
