9d7e33506c8a6b0003a655e6da9dfeab29307d24
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-07-27 21:06:31 +0100

    generate ics files

---
+++ src/pages/events/[eventId].ics.ts
@@ -0,0 +1,43 @@
+import type { APIRoute } from "astro";
+import { getCollection, getEntry } from "astro:content";
+
+const formatDate = (date: Date) =>
+  `${date.toISOString().replaceAll("-", "").replaceAll(":", "").replaceAll(".", "").slice(0, 15)}Z`;
+
+export const GET: APIRoute = async ({ params, redirect }) => {
+  const { eventId } = params;
+  if (eventId === undefined) {
+    return redirect("/404");
+  }
+
+  const entry = await getEntry("events", eventId);
+  if (entry === undefined) {
+    return redirect("/404");
+  }
+
+  return new Response(
+    `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:nn1.dev
+BEGIN:VEVENT
+DTSTART:${formatDate(entry.data.dateStart)}
+DTEND:${formatDate(entry.data.dateEnd)}
+DTSTAMP:${formatDate(new Date())}
+SUMMARY:NN1 Dev Club ${entry.data.name}
+DESCRIPTION:${entry.data.description}
+URL:https://nn1.dev/events/${eventId}
+LOCATION:${entry.data.locationText}
+UID:${entry.data.id}
+END:VEVENT
+END:VCALENDAR
+`,
+  );
+};
+
+export async function getStaticPaths() {
+  const allEvents = await getCollection("events");
+
+  return allEvents.map(({ data }) => ({
+    params: { eventId: data.id },
+  }));
+}
