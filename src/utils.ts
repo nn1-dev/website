a0e88982a9eb26cef79f9d996a36452c1029236b
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-08-02 05:51:50 +0100

    Remove outlook calendar links, they are not reliable

--- src/utils.ts
+++ src/utils.ts
@@ -22,44 +22,44 @@ const getInviteUrls = ({
     second: date.getUTCSeconds().toString().padStart(2, "0"),
   });
 
-  type Part = keyof ReturnType<typeof getUtcParts>;
+  // type Part = keyof ReturnType<typeof getUtcParts>;
 
-  const localFormat = new Intl.DateTimeFormat("en-GB", {
-    timeZone: "Europe/London",
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    hour12: false,
-  });
-  const getLocalParts = (date: Date) =>
-    (localFormat.formatToParts(date) as { type: Part; value: string }[]).reduce(
-      (acc, part) => {
-        if (
-          ["year", "month", "day", "hour", "minute", "second"].includes(
-            part.type,
-          )
-        ) {
-          acc[part.type] = part.value;
-        }
-        return acc;
-      },
-      {} as Record<Part, string>,
-    );
+  // const localFormat = new Intl.DateTimeFormat("en-GB", {
+  //   timeZone: "Europe/London",
+  //   year: "numeric",
+  //   month: "2-digit",
+  //   day: "2-digit",
+  //   hour: "2-digit",
+  //   minute: "2-digit",
+  //   second: "2-digit",
+  //   hour12: false,
+  // });
+  // const getLocalParts = (date: Date) =>
+  //   (localFormat.formatToParts(date) as { type: Part; value: string }[]).reduce(
+  //     (acc, part) => {
+  //       if (
+  //         ["year", "month", "day", "hour", "minute", "second"].includes(
+  //           part.type,
+  //         )
+  //       ) {
+  //         acc[part.type] = part.value;
+  //       }
+  //       return acc;
+  //     },
+  //     {} as Record<Part, string>,
+  //   );
 
   const dateTimeStartPartsUtc = getUtcParts(dateStart);
-  const dateTimeStartPartsLocal = getLocalParts(dateStart);
+  // const dateTimeStartPartsLocal = getLocalParts(dateStart);
 
   const dateTimeEndPartsUtc = getUtcParts(dateEnd);
-  const dateTimeEndPartsLocal = getLocalParts(dateEnd);
+  // const dateTimeEndPartsLocal = getLocalParts(dateEnd);
 
   const googleStart = `${dateTimeStartPartsUtc.year}${dateTimeStartPartsUtc.month}${dateTimeStartPartsUtc.day}T${dateTimeStartPartsUtc.hour}${dateTimeStartPartsUtc.minute}${dateTimeStartPartsUtc.second}Z`;
   const googleEnd = `${dateTimeEndPartsUtc.year}${dateTimeEndPartsUtc.month}${dateTimeEndPartsUtc.day}T${dateTimeEndPartsUtc.hour}${dateTimeEndPartsUtc.minute}${dateTimeEndPartsUtc.second}Z`;
 
-  const outlookStart = `${dateTimeStartPartsLocal.year}-${dateTimeStartPartsLocal.month}-${dateTimeStartPartsLocal.day}T${dateTimeStartPartsLocal.hour}%3A${dateTimeStartPartsLocal.minute}%3A${dateTimeStartPartsLocal.second}`;
-  const outlookEnd = `${dateTimeEndPartsLocal.year}-${dateTimeEndPartsLocal.month}-${dateTimeEndPartsLocal.day}T${dateTimeEndPartsLocal.hour}%3A${dateTimeEndPartsLocal.minute}%3A${dateTimeEndPartsLocal.second}`;
+  // const outlookStart = `${dateTimeStartPartsLocal.year}-${dateTimeStartPartsLocal.month}-${dateTimeStartPartsLocal.day}T${dateTimeStartPartsLocal.hour}%3A${dateTimeStartPartsLocal.minute}%3A${dateTimeStartPartsLocal.second}`;
+  // const outlookEnd = `${dateTimeEndPartsLocal.year}-${dateTimeEndPartsLocal.month}-${dateTimeEndPartsLocal.day}T${dateTimeEndPartsLocal.hour}%3A${dateTimeEndPartsLocal.minute}%3A${dateTimeEndPartsLocal.second}`;
 
   const encodedName = encodeURIComponent(`NN1 Dev Club ${name}`);
   const encodedDescription = encodeURIComponent(description);
@@ -68,8 +68,8 @@ const getInviteUrls = ({
   return {
     iCal: `https://nn1.dev/events/${id}/event.ics`,
     google: `https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${googleStart}%2F${googleEnd}&details=${encodedDescription}&location=${encodedLocation}&text=${encodedName}`,
-    outlook: `https://outlook.live.com/calendar/0/action/compose?allday=false&body=${encodedDescription}&enddt=${outlookEnd}&location=${encodedLocation}&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${outlookStart}&subject=${encodedName}`,
-    office365: `https://outlook.office.com/calendar/0/action/compose?allday=false&body=${encodedDescription}&enddt=${outlookEnd}&location=${encodedLocation}&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${outlookStart}&subject=${encodedName}`,
+    // outlook: `https://outlook.live.com/calendar/0/action/compose?allday=false&body=${encodedDescription}&enddt=${outlookEnd}&location=${encodedLocation}&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${outlookStart}&subject=${encodedName}`,
+    // office365: `https://outlook.office.com/calendar/0/action/compose?allday=false&body=${encodedDescription}&enddt=${outlookEnd}&location=${encodedLocation}&path=%2Fcalendar%2Faction%2Fcompose&rru=addevent&startdt=${outlookStart}&subject=${encodedName}`,
   };
 };
 
