d33bf384429af42f021a21c93da29b2aa73dcb1f
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-02-07 19:52:18 +0000

    Remove confusing timing description from the seo description

--- src/constants.ts
+++ src/constants.ts
@@ -21,4 +21,4 @@ export const API_HEADERS_FEEDBACK = {
 export const SEO_TITLE =
   "NN1 Dev Club - free meet-up for developers from Northampton";
 export const SEO_DESCRIPTION =
-  "A free, quarterly meet-up for a local community of Northamptonshire-based software developers. A place to meet, collaborate and share knowledge with each other.";
+  "A free meet-up for a local community of Northamptonshire-based software developers. A place to meet, collaborate and share knowledge with each other.";
