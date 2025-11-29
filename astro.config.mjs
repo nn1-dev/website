73ad912eed499aa8ba33277ec2e70ec804696d68
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2025-11-29 06:56:07 +0000

    move to cf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig, envField } from "astro/config";
-import netlify from "@astrojs/netlify";
+import cloudflare from "@astrojs/cloudflare";
 import sentry from "@sentry/astro";
 import fs from "node:fs";
 import path from "node:path";
@@ -8,7 +8,6 @@ import sitemap from "@astrojs/sitemap";
 
 const EXCLUDED_ROUTES = ["https://nn1.dev/newsletter/unsubscribe/"];
 
-//TODO: Check if this stil works after migration to Astro 5
 const events = fs
   .readdirSync("./src/data/events")
   .map((file) => path.basename(file, path.extname(file)));
@@ -16,7 +15,7 @@ const events = fs
 // https://astro.build/config
 export default defineConfig({
   site: "https://nn1.dev",
-  adapter: netlify(),
+  adapter: cloudflare(),
   env: {
     schema: {
       API_KEY_TICKETS: envField.string({
@@ -39,9 +38,7 @@ export default defineConfig({
   integrations: [
     sitemap({
       filter: (page) => !EXCLUDED_ROUTES.includes(page),
-      //TODO: Is this still needed, potentially not, it may be a legacy
-      //since we used to render events apges on the server, not they are statically rendered
-      customPages: events.map((event) => `https://nn1.dev/events/${event}/`),
+      // customPages: events.map((event) => `https://nn1.dev/events/${event}/`),
     }),
   ],
 });
